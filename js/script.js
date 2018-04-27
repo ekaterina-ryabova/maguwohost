819efd5d2d247dd1db10a4841ed32870057483fe
Ekaterina Ryabova <ekaterina.ryabova@sokolov.ru>
2018-04-27 09:12:45 +0300

    Инициализация репозитория

---
+++ js/script.js
@@ -0,0 +1,7 @@
+﻿"use strict";
+	var showMenu = document.getElementById('menu-show-button');
+	var navigation = document.getElementById('mobile-navigation');
+
+	showMenu.addEventListener('click', function() {
+		navigation.classList.toggle('menu-panel--active');
+	});
